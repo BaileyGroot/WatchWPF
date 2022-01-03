883ca614307d41659de85bc57f049d626fa320e0
Markus Bailey Stockfleth Thomsbæk Groot <mark4805@zbc.dk>
2022-01-03 23:08:32 +0100

    Add project files.

---
+++ WatchWPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WatchWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    // WPF Xaml
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer _timer;
+        TimeSpan _time;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _time = TimeSpan.FromSeconds(100);
+
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                tbTime.Text = _time.ToString("c");
+                if (_time == TimeSpan.Zero) _timer.Stop();
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Start();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+
+        private void SetTimer_Click(object sender, RoutedEventArgs e)
+        {
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Boxed.Text = number.ToString();
+        }
+    }
+}
